1e4753196ec5f4b8da40cdca51f8121a1c6f0678
chiquillo7777 <carlosjosechiquillolopez@gmail.com>
2020-01-27 17:46:59 +0000

    creating calss CustomerOrdersWsImpl which will implement CustomerOrdersPortType interface

---
+++ src/main/java/com/carloschiquillo/ws/soap/CustomerOrdersWsImpl.java
@@ -0,0 +1,18 @@
+package com.carloschiquillo.ws.soap;
+
+import com.bharath.ws.trainings.*;
+
+public class CustomerOrdersWsImpl implements CustomerOrdersPortType {
+
+    @Override
+    public GetOrdersResponse getOrders(GetOrdersRequest parameters) {
+        return null;
+    }
+
+    @Override
+    public CreateOrdersResponse createOrders(CreateOrdersRequest parameters) {
+        return null;
+    }
+    
+}
+
